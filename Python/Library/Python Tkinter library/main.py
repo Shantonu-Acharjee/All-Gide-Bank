78cfdbe06c433e2eae8b220427a46fd0e85037c7
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-02-19 12:33:12 +0600

    Video 5 Done

--- Python/Library/Python Tkinter library/main.py
+++ Python/Library/Python Tkinter library/main.py
@@ -69,3 +69,50 @@ root.mainloop()
 """
 
 # Video 3 Done.
+
+'''
+
+from tkinter import*
+root = Tk()
+
+name = Label(root, text='name')
+password = Label(root, text='Password')
+
+entry1 = Entry(root)
+entry2 = Entry(root)
+
+
+
+name.grid(row = 0, column= 0, sticky= E)
+entry1.grid(row = 0, column= 1)
+
+password.grid(row = 1, column= 0)
+entry2.grid(row = 1, column= 1)
+
+c = Checkbutton(root, text='Keep me logged in')
+c.grid(columnspan=2)
+
+
+root.mainloop()
+
+'''
+
+# Video 4 Done 
+
+
+'''
+
+from tkinter import *
+root = Tk()
+
+def Cll_me():
+    print('Shantonu Acharjee')
+
+button = Button(root, text= 'Click Me', command= Cll_me)
+
+button.pack()
+root.mainloop()
+
+'''
+
+# Video 5 Done
