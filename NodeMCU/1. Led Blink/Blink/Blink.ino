bf91824a25c4d5452cddc115eccf67198a7fe347
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-03-10 18:08:41 +0600

    Node Mcu Add Done

---
+++ NodeMCU/1. Led Blink/Blink/Blink.ino
@@ -0,0 +1,14 @@
+#define LED D7 // LED IN D7
+
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED, HIGH);   
+  delay(1000);                     
+  digitalWrite(LED, LOW);    
+  delay(1000);                       
+}
